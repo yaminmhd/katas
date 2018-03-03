c275fd312d2628b2fe3491f5a0ca7ee91b841df9
yaminmhd <mdyamin.drzee@gmail.com>
2018-03-03 14:21:50 +0800

    Added alternative answer to find-first-non-consecutive-number.js

--- find-first-non-consecutive-number.js
+++ find-first-non-consecutive-number.js
@@ -21,3 +21,17 @@ function firstNonConsecutive (arr) {
  }
  return null;
 }
+
+
+/*
+Alternative answer
+  function firstNonConsecutive(arr) {
+  for (let i = 0; i < arr.length - 1; ++i) {
+    if (arr[i] + 1 !== arr[i + 1]) {
+      return arr[i + 1]
+    }
+  }
+  return null
+}
+
+*/
